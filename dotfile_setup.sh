00fd30b1799185c60f197ba44a2fd52eb7911deb
Michael <michael@xostrucks.com>
2020-03-03 21:31:20 -0800

    ADD: dotfile setup

---
+++ dotfile_setup.sh
@@ -0,0 +1,16 @@
+rm ~/.bashrc -f
+ln -s ~/dotfiles/.bashrc ~
+
+rm ~/.bash_aliases -f
+ln -s ~/dotfiles/.bash_aliases ~
+
+rm ~/.gitconfig -f
+ln -s ~/dotfiles/.gitconfig ~
+
+rm ~/.vimrc -f
+ln -s ~/dotfiles/.vimrc ~
+
+rm ~/.tmux.conf -f
+ln -s ~/dotfiles/.tmux.conf ~
+
+
