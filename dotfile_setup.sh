834f4da24096cd704c329195ebce87c995ad1a4b
Michael <michael@xostrucks.com>
2021-03-06 10:46:11 -0800

    ADD: Clone zsh plugins

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -16,3 +16,6 @@ ln -s ~/dotfiles/.tmux.conf ~
 rm ~/.zshrc -f
 ln -s ~/dotfiles/.zshrc ~
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
