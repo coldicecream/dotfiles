a1ae3c848572c2604f8f7b242cb6d8d5cf33c106
Michael <michael@xostrucks.com>
2021-10-03 20:37:49 -0700

    ADD: Install Oh-my-zsh

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -16,6 +16,18 @@ ln -s ~/dotfiles/.tmux.conf ~
 rm ~/.zshrc -f
 ln -s ~/dotfiles/.zshrc ~
 
+
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+
+git clone https://github.com/powerline/fonts.git ~/fonts
+cd ~/fonts
+./install.sh
+
+git clone https://github.com/gibbling666/dircolors.git ~/.dircolors
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
